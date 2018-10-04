216262fa622d49a073ad8fca806f0abd2b98ff2b
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2018-10-04 18:52:56 +0200

    Map assets added

---
+++ Mapa/tiles_spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_spritesheet" tilewidth="70" tileheight="70" spacing="2" tilecount="156" columns="12">
+ <image source="../Documents/GitHub/Development-Platform-Game/platformerGraphicsDeluxe_Updated/Tiles/tiles_spritesheet.png" width="862" height="934"/>
+</tileset>
