ae93f74e8ac47eb4f4ecf10611660e35630cd1a0
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2018-10-05 18:27:11 +0200

    map works

--- PlatformGame/Game/maps/items_spritesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="items_spritesheet" tilewidth="70" tileheight="70" spacing="2" tilecount="64" columns="8">
- <image source="items_spritesheet.png" width="576" height="576"/>
-</tileset>
--- PlatformGame/j1Scene.cpp
+++ PlatformGame/j1Scene.cpp
@@ -30,7 +30,7 @@ bool j1Scene::Awake()
 // Called before the first frame
 bool j1Scene::Start()
 {
-	App->map->Load("sin nombre.tmx");
+	App->map->Load("proba.tmx");
 	return true;
 }
 
