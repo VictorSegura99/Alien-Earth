fca88d312707c30dafb418d4a27c0fee037ca10c
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2018-10-05 10:37:01 +0200

    Links modified

---
+++ PlatformGame/Game/maps/preview.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="preview" tilewidth="70" tileheight="70" spacing="2" tilecount="81" columns="9">
+ <image source="../Documents/GitHub/Development-Platform-Game/platformerGraphicsDeluxe_Updated/preview.png" trans="a2e7ee" width="698" height="675"/>
+</tileset>
