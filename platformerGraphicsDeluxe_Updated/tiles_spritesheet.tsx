70ce80ea836d2039ff8a083e8be51118022f95d3
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2018-10-06 14:44:49 +0200

    A lot of sprites added+modified and Map 1 Finished(at the moment)

--- platformerGraphicsDeluxe_Updated/tiles_spritesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles_spritesheet" tilewidth="72" tileheight="72" tilecount="132" columns="11">
- <image source="Tiles/tiles_spritesheet.png" width="862" height="934"/>
-</tileset>
