17c4787a78588d9b641b59e04fe88f090007b4a6
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2018-10-12 11:02:46 +0200

    fixing colliders

--- PlatformGame/j1Map.cpp
+++ PlatformGame/j1Map.cpp
@@ -175,7 +175,7 @@ bool j1Map::Load(const char* file_name)
 		i = prop.attribute("value").as_int();
 
 		if (i == 1) {
-			App->collision->AddCollider(rect, COLLIDER_TYPE::COLLIDER_WALL);
+			App->collision->AddCollider(rect, COLLIDER_TYPE::COLLIDER_WALL_LEFT);
 		}
 		else if(i==2) {
 			App->collision->AddCollider(rect, COLLIDER_TYPE::COLLIDER_GROUND);
@@ -191,6 +191,9 @@ bool j1Map::Load(const char* file_name)
 		}
 		else if (i == 6) {
 			App->collision->AddCollider(rect, COLLIDER_TYPE::COLLIDER_WATER);
+		}
+		else if (i == 7) {
+			App->collision->AddCollider(rect, COLLIDER_TYPE::COLLIDER_WALL_RIGHT);
 		}
 	
 		
--- PlatformGame/jPlayer.h
+++ PlatformGame/jPlayer.h
@@ -52,10 +52,14 @@ public:
 	Animation idle2;
 	Animation GoLeft;
 	Animation GoRight;
-	Animation Jump;
+	Animation jump;
 	Animation SwimRight;
 	Animation SwimLeft;
 	Animation Climb;
+	bool WalkLeft = false;
+	bool WalkRight = false;
+	bool Idle = false;
+	bool Jump = false;
 	fPoint position;
 	bool IsJumping = false;
 	uint KnowState;
@@ -67,7 +71,6 @@ public:
 	SDL_Texture* texture = nullptr;
 	p2SString sprites_name;
 	bool NextMap = false;
-	bool anime = true;
 	int positionWinMap1 = 10780;
 	int startpointcameramap2 = -60;
 	float gravity = -4.8f;
