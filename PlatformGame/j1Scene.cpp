3b71a6d270917f1824f2409e27e660857bd621a6
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2018-10-14 22:51:37 +0200

    title added

--- PlatformGame/j1Scene.cpp
+++ PlatformGame/j1Scene.cpp
@@ -104,10 +104,8 @@ bool j1Scene::Update(float dt)
 
 
 	// "Map:%dx%d Tiles:%dx%d Tilesets:%d"
-	p2SString title("Map:%dx%d Tiles:%dx%d Tilesets:%d",
-					App->map->data.width, App->map->data.height,
-					App->map->data.tile_width, App->map->data.tile_height,
-					App->map->data.tilesets.count());
+	p2SString title("Alien Earth");
+					
 
 	App->win->SetTitle(title.GetString());
 	return true;
